695f7bf7124359c54260fd6db3e32d7fcc790b44
Oleh Chornyi <ochernyi04@gmail.com>
2023-08-26 20:50:26 +0300

    Register models in admin.py

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from airport.models import (
+    Airport,
+    Route,
+    Crew,
+    AirplaneType,
+    Airplane,
+    Flight,
+    Order,
+    Ticket,
+)
+
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInline,)
+
+
+admin.site.register(Airport)
+admin.site.register(Route)
+admin.site.register(Crew)
+admin.site.register(AirplaneType)
+admin.site.register(Airplane)
+admin.site.register(Flight)
+admin.site.register(Ticket)
