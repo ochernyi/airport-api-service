5a0c31f9cf6320c9ec861e135fe330e703b48e09
Oleh Chornyi <ochernyi04@gmail.com>
2023-08-26 20:54:02 +0300

    Add routers to project

--- airport/urls.py
+++ airport/urls.py
@@ -1,10 +1,25 @@
-from rest_framework.routers import SimpleRouter
+from django.urls import path, include
+from rest_framework import routers
 
+from airport.views import (
+    AirportViewSet,
+    RouteViewSet,
+    CrewViewSet,
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    FlightViewSet,
+    OrderViewSet,
+)
 
-router = SimpleRouter()
-# router.register(r"news", NewsViewSet, basename="airport")
+router = routers.DefaultRouter()
+router.register("airports", AirportViewSet)
+router.register("routes", RouteViewSet)
+router.register("crews", CrewViewSet)
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("flights", FlightViewSet)
+router.register("orders", OrderViewSet)
 
-
-urlpatterns = router.urls
+urlpatterns = [path("", include(router.urls))]
 
 app_name = "airport"
